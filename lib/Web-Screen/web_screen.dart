b10b7349f4a10142b316d10c8968819f3116b887
thenewdevelop <mohamedafifi89999@gmail.com>
2021-10-10 16:52:21 +0200

    material button

--- lib/Web-Screen/web_screen.dart
+++ lib/Web-Screen/web_screen.dart
@@ -62,6 +62,7 @@ class WebScreenResponsive extends StatelessWidget {
     );
   }
 
+  //Material Button
   MaterialButton buildMaterialButton({required String label}) {
     return MaterialButton(
       height: 60.0,
